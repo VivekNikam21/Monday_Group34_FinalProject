6516a75caaadc6330bb30b8b4f6fe1cf8f2e38a1
nehamahangade <nehamahangade8@gmail.com>
2024-12-07 19:26:01 -0500

    Content JPanel created

--- Group34FinalProject/src/ui/Therapist/TherapistWorkAreaJpanel.java
+++ Group34FinalProject/src/ui/Therapist/TherapistWorkAreaJpanel.java
@@ -27,34 +27,78 @@ public class TherapistWorkAreaJpanel extends javax.swing.JPanel {
     private void initComponents() {
 
         lblTherapist = new javax.swing.JLabel();
+        btnPatientRecords = new javax.swing.JButton();
+        btnSessionManagement = new javax.swing.JButton();
+        btnBack = new javax.swing.JButton();
 
         setBackground(new java.awt.Color(0, 102, 102));
 
-        lblTherapist.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        lblTherapist.setFont(new java.awt.Font("Segoe UI", 1, 48)); // NOI18N
         lblTherapist.setForeground(new java.awt.Color(255, 255, 255));
         lblTherapist.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         lblTherapist.setText("THERAPIST");
+
+        btnPatientRecords.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        btnPatientRecords.setForeground(new java.awt.Color(0, 102, 102));
+        btnPatientRecords.setText("Patient Records");
+        btnPatientRecords.setMaximumSize(new java.awt.Dimension(280, 50));
+
+        btnSessionManagement.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        btnSessionManagement.setForeground(new java.awt.Color(0, 102, 102));
+        btnSessionManagement.setText("Session Management");
+
+        btnBack.setBackground(new java.awt.Color(204, 255, 255));
+        btnBack.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        btnBack.setForeground(new java.awt.Color(0, 102, 102));
+        btnBack.setText("B A C K");
+        btnBack.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnBackActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(16, 16, 16)
-                .addComponent(lblTherapist, javax.swing.GroupLayout.PREFERRED_SIZE, 697, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(50, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(157, 157, 157)
+                        .addComponent(btnPatientRecords, javax.swing.GroupLayout.PREFERRED_SIZE, 295, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(317, 317, 317)
+                        .addComponent(btnSessionManagement, javax.swing.GroupLayout.PREFERRED_SIZE, 295, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(16, 16, 16)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(btnBack, javax.swing.GroupLayout.PREFERRED_SIZE, 155, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(lblTherapist, javax.swing.GroupLayout.PREFERRED_SIZE, 1196, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(708, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(19, 19, 19)
-                .addComponent(lblTherapist, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(419, Short.MAX_VALUE))
+                .addComponent(lblTherapist, javax.swing.GroupLayout.PREFERRED_SIZE, 62, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(84, 84, 84)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnSessionManagement, javax.swing.GroupLayout.PREFERRED_SIZE, 189, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnPatientRecords, javax.swing.GroupLayout.PREFERRED_SIZE, 189, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 646, Short.MAX_VALUE)
+                .addComponent(btnBack, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(23, 23, 23))
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btnBackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBackActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnBackActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnBack;
+    private javax.swing.JButton btnPatientRecords;
+    private javax.swing.JButton btnSessionManagement;
     private javax.swing.JLabel lblTherapist;
     // End of variables declaration//GEN-END:variables
 }
